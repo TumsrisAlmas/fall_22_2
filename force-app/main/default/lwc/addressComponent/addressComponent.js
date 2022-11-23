35fd128eaefd017b370fbd75f8a358ae485e60d1
bakaisf <bakaisf@gmail.com>
2022-11-23 08:19:19 +0530

    22NOV2022

---
+++ force-app/main/default/lwc/addressComponent/addressComponent.js
@@ -0,0 +1,47 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import getAddress from '@salesforce/apex/AddressController.getAddress';
+
+
+export default class AddressComponent extends LightningElement {
+    @api zipCode; //Receiver.
+    @track contactAddresses;
+    @track error; //Decorator
+    @track selectedContact;
+    @track selectedIndex;
+
+    @wire(getAddress, {zipCode:'$zipCode'}) //Parameterized wire method, meaning its sending data or info to the Apex controller.
+    wiredContacts({ error, data }) {
+        if (data) {
+            console.log('DATA', data);
+            this.contactAddresses = data;
+            this.error = undefined;
+        } else if (error) {
+            console.log('DATA', error);
+            this.error = error;
+            this.contactAddresses = undefined;
+        }
+    }
+
+    handleButtonClick(event) { 
+        console.log('INDEX', event.target.dataset.index);
+        this.selectedIndex = event.target.dataset.index;
+
+        this.selectHandler(event);
+    }
+
+
+    selectHandler(event) {
+        // Prevents the anchor element from navigating to a URL.
+        event.preventDefault();
+
+        // Creates the event with the contact ID data.
+        const selectedEvent = new CustomEvent(
+            'contactselcted',
+            {
+                detail: this.contactAddresses[this.selectedIndex]
+            });
+
+        // Dispatches the event.
+        this.dispatchEvent(selectedEvent);
+    }
+}
